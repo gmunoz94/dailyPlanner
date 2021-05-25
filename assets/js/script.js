7999e9f0e98855d92e0886ab879bf1548564ef8b
gmunoz94 <g.munozii@icloud.com>
2021-05-24 21:51:51 -0500

    added row w/ time via JQuery loop, and styling to each row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,35 @@
-var time = ["9 a", "10 a", "11 a", "12 A", "1 p", "2 p", "3 p", "4 p", "5 p"]
+var timeDisplayEl = $('#time-display');
+var timeBlock = $("<div></div>").addClass('col-2 col-md-1 text-center time-block')
+var textArea = $("<textarea></textarea>").addClass('col-6 col-md-6 bg-dark text-white')
+var saveBlock = $("<a></a>").addClass('col-2 col-md-1 text-center align-middle save-block').html('<a class="bi bi-save-fill"></a')
 
-var firstTime = moment(time[i], "h a").format("h a")
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+}
 
-$("#9am").text(firstTime);
+setInterval(displayTime, 1000);
 
-var tRowHead = $("#timeTable tbody tr")
+var time = ["9 a", "10 a", "11 a", "12 a", "1 p", "2 p", "3 p", "4 p", "5 p"]
 
-tRowHead.find('td:last').each(function() {
-    $(this).html('<a class="bi bi-save-fill"></a>')
-    $(this).addClass('align-right')
-})
+var j = 0
+for(j = 0; j < time.length; j++) {
+    $('section').append('<div class="row col-12 d-flex justify-content-center m-2"></div>');
+}
 
+addRow()
 
+function addRow() {
+$('section div').append(timeBlock)
+$('section div').append(textArea)
+$('section div').append(saveBlock)
+addTime()
+}
 
-var i = 0;
-$('table tr').not(':first').find('th:first').each(function () {
-
-    $(this).text(moment(time[i], "h a").format("h a"))
-    i++;
-})
-
+function addTime() {
+    var i = 0;
+    $('body section div').find('div:first').each(function () {
+        $(this).text(moment(time[i], "h a").format("ha"))
+        i++;
+    })
+}
